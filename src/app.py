8a24b371040ccbe85c02fdbaac3b2172e048adce
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-06-24 11:27:05 +0000

    añadimos DETELE position 323 para borrar todas las tareas

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 todos = [ { "label": "My first task", "done": False } ]
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
+def get_todos():
     return jsonify(todos)
 
 @app.route('/todos', methods=['POST'])
@@ -16,13 +17,17 @@ def add_new_todo():
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
+    global todos
     print("This is the position to delete:", position)
-    
-    todos.pop(position)
+
+    if position == 323:
+        todos = []
+    else:
+        if position < 0 or position >= len(todos):
+            return jsonify({"error": "Invalid position"}), 400
+        todos.pop(position)
     
     return jsonify(todos)
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
